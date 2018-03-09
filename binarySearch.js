bde15196b202b85e90677e32f3db2c46e143fb5e
lixun <1261142605@qq.com>
2018-03-09 15:47:23 +0800

    first commit

---
+++ binarySearch.js
@@ -0,0 +1,22 @@
+function binary_search(list, item) {
+  let low = 0
+  let high = list.length - 1
+  while (low <= high) {
+    const mid = low + high 
+    const guess = list[mid]
+    if (guess == item) {
+        return mid
+    } 
+    if (guess > item) {
+        high = mid - 1
+    } else {
+        low = mid + 1
+    }
+  }
+  return null
+}
+
+const array = [1,2,3,4,5,7,8,9]
+console.log(binary_search(array, 4))
+
+
